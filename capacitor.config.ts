e2a5405dde4491ac4b0b099602be6f19abc8e382
Denis Akira Ise Washio <denis@readyon.ai>
2023-09-26 12:45:42 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'coding.prompt',
+  appName: 'coding-prompt',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
